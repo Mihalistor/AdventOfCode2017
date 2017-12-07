9784e87dad3a1c6b7743b8e5be48668323722fda
Bruno Mihalić <bruno.mihalic@hotmail.com>
2017-12-07 19:51:35 +0100

    Rješenje zadatka 2
    Uspoređuje se broj i broj udaljen određeni broj mjesta (varijabla jump).

---
+++ Dan 1/day1-2.java
@@ -0,0 +1,24 @@
+List<Integer> numberList = new ArrayList<>();
+Scanner scanner = new Scanner(new File("input.txt"));
+scanner.useDelimiter("");
+while (scanner.hasNext()) {
+    String s = scanner.next();
+    numberList.add(Integer.parseInt(s));
+}
+int jump = numberList.size()/2;
+int sum = 0;
+for (int i = 0; i < numberList.size() - jump; i++) {
+    if (numberList.get(i).equals(numberList.get(i + jump))) {
+        sum += numberList.get(i);
+    }
+}
+List<Integer> part1 = numberList.subList(0,numberList.size()-jump);
+List<Integer> part2 = numberList.subList(numberList.size()-jump, numberList.size());
+List<Integer> newNumberList = new ArrayList<>(part2);
+newNumberList.addAll(part1);
+for (int i = 0; i < newNumberList.size() - jump; i++) {
+    if (newNumberList.get(i).equals(newNumberList.get(i + jump))) {
+        sum += newNumberList.get(i);
+    }
+}
+System.out.println("SUM: " + sum);
