a1ea893a0babf3df31a9468dbc2dc868a9a56fc0
Bruno Mihalić <bruno.mihalic@hotmail.com>
2017-12-07 21:49:32 +0100

    Rješenje zadatka 2

---
+++ Day 2 - Corruption Checksum/day2-2.java
@@ -0,0 +1,19 @@
+Scanner scanner = new Scanner(new File("input.txt"));
+int sum = 0;
+while (scanner.hasNextLine()) {
+	String[] row = scanner.nextLine().split("\\t");
+	List<String> helper = Arrays.asList(row);
+	List<Integer> numbers = new ArrayList<>();
+	for (String number : helper) {
+		numbers.add(Integer.parseInt(number));
+	}
+    Collections.sort(numbers);
+    for (int j = numbers.size() - 1; j > 0; j--) {
+        for (int i = j-1; i >= 0; i--) {
+            if (numbers.get(j) % numbers.get(i) == 0) {
+                sum += numbers.get(j) / numbers.get(i);
+            }
+        }
+    }
+}
+System.out.println("SUM: " + sum);
