f50a1c261ce51e592f03335fd7a5d808f3ffd0c1
Bruno Mihalić <bruno.mihalic@hotmail.com>
2017-12-11 16:19:02 +0100

    Rješenje zadatka 1 za dan 6
    Prvo se u listu integera spremaju vrijednosti, zatim se u beskonačnoj while petlji provjerava max broj iz liste i određuje se broj (number) koji se dodjeljuje ostalim brojevima u listi. Pomoću dvije for petlje prolazi se od max broja do kraja liste te od pocetka do max broja i svakom broju se dodaje number. U counter se sprema ukupni broj koji je dodan i ako je jednak max tada se prekida petlja. Ako je counter manji od max tada se na mijestu max broja dodaje razlika ili se zapiše nula. Na kraju se svi brojevi iz liste spremaju u string koji se sprema  u listu stringova. While petlja se prekida kada se kreira blok koji već postoji u listi stringova. Rješenje je broj ciklusa do pojave postojećeg bloka.

---
+++ Day 6 - Memory Reallocation/day6-1.java
@@ -0,0 +1,35 @@
+Scanner scanner = new Scanner(new File("input.txt"));
+List<String> historyOfCycles = new ArrayList<>();
+String[] row = scanner.nextLine().split("\\t");
+List<String> helper = Arrays.asList(row);
+List<Integer> banks = new ArrayList<>();
+for (String block : helper) {
+	banks.add(Integer.parseInt(block));
+}
+while (true) {
+	String blocks = "";
+	int max = Collections.max(banks);
+	int index = banks.indexOf(max);
+	int number = max / (banks.size() - 1);
+	if(number == 0) number = 1;
+	int counter = 0;
+	for (int i = index + 1; i < banks.size(); i++) {
+		if(counter == max) break;
+		banks.set(i, banks.get(i) + number);
+		counter += number;
+	}
+	for (int i = 0; i < index; i++) {
+		if(counter == max) break;
+		banks.set(i, banks.get(i) + number);
+		counter += number;
+	}
+	if (counter < max) banks.set(index, max - counter);
+	else banks.set(index, 0);
+	for (int i = 0; i < banks.size(); i++) {
+		blocks += banks.get(i);
+	}
+	if (historyOfCycles.contains(blocks)) break;
+	historyOfCycles.add(blocks);
+}
+System.out.println("CYCLES: " + (historyOfCycles.size()+1));
+//RESULT IS: 3156
